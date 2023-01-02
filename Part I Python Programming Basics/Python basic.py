ecfe5c7c5a9e61adf83e0157a5d6a9735060e474
ezghou <www.egrenzgo@gmail.com>
2023-01-02 14:11:56 +0800

    Chapter 1: Python Basics

---
+++ Part I Python Programming Basics/Python basic.py
@@ -0,0 +1,42 @@
+# Convention START with lowerCase when naming a variable
+sum = 11 + 11
+print(sum) #22
+
+product = 11 * 11
+print(product) #121
+
+diff = 11 - 11
+print(diff) #0
+
+quotient = 12 / 11
+print(quotient)#1.090909090908
+
+quotient1 = 12 // 11
+print(quotient1)#1
+
+
+modulo = 112 % 11
+print(modulo)#2
+
+expo = 2 ** 11
+print(expo)#2048
+
+#Precedence  **,*, /, //, %, +, -, LEFT TO RIGHT
+
+#Integer NO DECIMAL POINT
+#FLOAT any num with decimal point
+#String with single quotation '', 'eg'
+
+
+#String Concat
+print('Alice' + 'Bob')
+print('Alice' + '2')
+
+#String Replication
+
+print('Alice' * 3)
+
+
+
+
+
