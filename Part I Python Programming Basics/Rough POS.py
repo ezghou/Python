6019f8e4a7a2e909a36c62c79967eb89e3685ebc
ezghou <www.egrenzgo@gmail.com>
2023-01-03 11:50:17 +0800

    Create Rough POS.py

---
+++ Part I Python Programming Basics/Rough POS.py
@@ -0,0 +1,19 @@
+# Write your code here :-)
+
+
+#Rough POS
+
+print('Welcome To Elizabeth Store!')
+
+print('What is your name?')
+name = input();
+print()
+print('Hello, ' + name + '. What I can help you with?')
+
+print('1. Buy')
+print('2. Inquire')
+print('3. Credit')
+choice = -1
+while(choice == -1):
+    print('Enter the number you want to proceed')
+    choice = input()
