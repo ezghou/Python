a935644dc9b65cd65ce6918fb928bc8c90322f8a
ezghou <www.egrenzgo@gmail.com>
2023-01-06 23:56:20 +0800

    Update Rock Paper Scissor.py
    Basic Rock Paper Scissor with ugly code

--- Part I Python Programming Basics/Rock Paper Scissor.py
+++ Part I Python Programming Basics/Rock Paper Scissor.py
@@ -1,1 +1,52 @@
-# Write your code here :-)
+import random
+
+print('Let us play Rock, Paper, Scissors!')
+
+
+while True:
+    chosen = random.randint(1,3)
+    print(chosen)
+    comp =''
+    guess = input()
+
+    if chosen == 1:
+        comp = 'Rock'
+        print(comp + ' vs. ' + guess)
+        if guess == 'Paper':
+            print('You beat me!')
+        elif guess == comp:
+            print('DRAW!')
+        else:
+            print('Nah, I win!')
+
+    elif chosen == 2:
+        comp = 'Paper'
+        print(comp + ' vs. ' + guess)
+        if guess == 'Scissors':
+            print('You beat me!')
+        elif guess == comp:
+            print('DRAW!')
+        else:
+            print('Nah, I win!')
+
+    else:
+        comp = 'Scissors'
+        print(comp + ' vs. ' + guess)
+        if guess == 'Rock':
+            print('You beat me!')
+        elif guess == comp:
+            print('DRAW!')
+        else:
+            print('Nah, I win!')
+
+    break
+
+
+
+
+
+
+
+
+
+
