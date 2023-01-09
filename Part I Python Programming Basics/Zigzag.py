44f1a2da7bc8247506b801e5abeff2b7fd1b2ff4
ezghou <www.egrenzgo@gmail.com>
2023-01-09 21:39:38 +0800

    Create Zigzag.py

---
+++ Part I Python Programming Basics/Zigzag.py
@@ -0,0 +1,45 @@
+# Write your code here :-)
+
+reset = 5
+count = 10
+while count != 0:
+    while reset != 0:
+        space = reset
+        while space != 0:
+            print(' ', end = '')
+            space -= 1
+        print('*****')
+
+        reset -= 1
+
+    reset += 1
+
+    while reset < 5:
+        bar = 5
+        reset += 1
+        space = reset
+        while space != 0:
+            print(' ', end = '')
+            space -= 1
+        print('*****')
+
+    reset -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    count -= 1
+
+
