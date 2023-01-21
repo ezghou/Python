8612033f7f45120635f9a3beea1b18686fb00d1d
ezghou <www.egrenzgo@gmail.com>
2023-01-21 21:22:40 +0800

    Update Character Picture Grid.py

--- Part I Python Programming Basics/Character Picture Grid.py
+++ Part I Python Programming Basics/Character Picture Grid.py
@@ -9,9 +9,17 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
 
-for y in range(6): #9
+for y in range(5): #9
     #print(len(grid[y]))
     for x in range(9):
+        print(grid[x][y], end = ' ')
+
+    print()
+
+
+for y in range(5, -1, -1): #9
+    #print(len(grid[y]))
+    for x in range(8, -1, -1):
         print(grid[x][y], end = ' ')
 
     print()
