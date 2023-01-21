a25f3aed61e192c93c92ea5b0a695de6799e86d5
ezghou <www.egrenzgo@gmail.com>
2023-01-21 21:16:59 +0800

    Update Coin Flip Streaks.py

--- Part I Python Programming Basics/Coin Flip Streaks.py
+++ Part I Python Programming Basics/Coin Flip Streaks.py
@@ -2,11 +2,11 @@ import random
 
 numberOfStreaks = 0
 
-for experimentNumber in range(10):
+for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
     list = []
 
-    for i in range(5):
+    for i in range(100):
         coin = random.randint(0,1)
 
         if coin == 0:
@@ -14,19 +14,31 @@ for experimentNumber in range(10):
         else:
             list.append('T')
 
-    print(list)
+#    print(list)
     # Code that checks if there is a streak of 6 heads or tails in a row.
     streakH = 0
     streakT = 0
     for i in range(len(list)-1):
-            if list[i] == list[i+1]:
-                streakH += 1
+        current = 'H'
+        if list[i] == current:
+            streakH += 1
+            if streakH == 6:
+                break
+        else:
+            streakH = 0
 
+    for i in range(len(list)-1):
+        current = 'H'
+        if list[i] == current:
+            streakH += 1
+            if streakH == 6:
+                break
+        else:
+            streakH = 0
 
-    print(streakH)
-
-
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+    numberOfStreaks += 1
+print(numberOfStreaks)
+print('Chance of streak: %s%%' % ((numberOfStreaks / 100000) * 100))
 
 
 
